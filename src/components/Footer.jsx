ce14b45afb63a7a495c26dedc1e0b046af61f91f
m3thx <mathieu.hatstatt@gmail.com>
2023-04-18 11:39:55 +0200

    18.04.23 Amélioration footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,126 +1,34 @@
 import styled from 'styled-components'
 
 import colors from '../styles/colors'
+import logo from '../assets/logo_white.png'
 
 const FooterContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;    
     background-color: ${colors.backgroundDark};
+    height:200px;
+    margin-top: 50px;
+    
+    padding-top: 65px;
+
+    p {
+        font-size: 1.5rem;
+        color: ${colors.white};
+        padding-top: 30px;
+    }
 `
 
 function Footer() {
     return (
         <FooterContainer>
-            <h1>Kasa</h1>
+            <img src={logo} alt="Kasa logo" />
+            <p>© 2020 Kasa. All rights reserved</p>
         </FooterContainer>
 
     )
 }
 
 export default Footer
-
-// /* D_Footer */
-
-// position: relative;
-// width: 1440px;
-// height: 209px;
-
-// background: #000000;
-
-
-// /* .LOGO */
-
-// position: absolute;
-// left: 45.76 %;
-// right: 45.76 %;
-// top: 31.58 %;
-// bottom: 49.55 %;
-
-
-
-// /* Vector */
-
-// position: absolute;
-// left: 0 %;
-// right: 74.17 %;
-// top: 0 %;
-// bottom: 8.45 %;
-
-// background: #FFFFFF;
-
-
-// /* Vector */
-
-// position: absolute;
-// left: 56.48 %;
-// right: 24.78 %;
-// top: 21.53 %;
-// bottom: 7.89 %;
-
-// background: #FFFFFF;
-
-
-// /* Vector */
-
-// position: absolute;
-// left: 80.56 %;
-// right: 0 %;
-// top: 21.25 %;
-// bottom: 7.9 %;
-
-// background: #FFFFFF;
-
-
-// /* Group */
-
-// position: absolute;
-// left: 28.63 %;
-// right: 49.47 %;
-// top: 21.53 %;
-// bottom: 0 %;
-
-
-
-// /* Vector */
-
-// position: absolute;
-// left: 43.52 %;
-// right: 53.94 %;
-// top: 68.66 %;
-// bottom: 8.17 %;
-
-// background: #FFFFFF;
-
-
-// /* Vector */
-
-// position: absolute;
-// left: 28.63 %;
-// right: 49.47 %;
-// top: 21.53 %;
-// bottom: 0 %;
-
-// background: #FFFFFF;
-
-
-// /* © 2020 Kasa. All rights reserved */
-
-// position: absolute;
-// left: 32.15 %;
-// right: 32.22 %;
-// top: 64.11 %;
-// bottom: 13.88 %;
-
-// font - family: 'Montserrat';
-// font - style: normal;
-// font - weight: 500;
-// font - size: 24px;
-// line - height: 142.6 %;
-// /* or 34px */
-// display: flex;
-// align - items: flex - end;
-// text - align: center;
-
-// color: #FFFFFF;
 
