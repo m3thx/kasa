b888adc76ec7236e58e87e95e35a84c919116467
m3thx <mathieu.hatstatt@gmail.com>
2023-04-29 02:00:37 +0200

    29.04.23 Composant Footer et style

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import logo from '../../assets/logo_white.png'
+import { FooterContainer } from './style'
+
+
+function Footer() {
+    return (
+        <FooterContainer>
+            <img src={logo} alt="Kasa logo" />
+            <p>© 2020 Kasa. All rights reserved</p>
+        </FooterContainer>
+
+    )
+}
+
+export default Footer
+
---
+++ src/components/Footer/style.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+import colors from '../../styles/colors'
+
+
+export const FooterContainer = styled.footer`
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;    
+    background-color: ${colors.backgroundDark};
+    min-height:200px;
+    margin-top: auto;
+    padding-top: 65px;
+    margin-left: 0;
+
+    p {
+        font-size: 1.5rem;
+        color: ${colors.white};
+        padding-top: 30px;
+    }
+`
