aa3cec4bc8764663774cdd7a63f6c9c2d717deb2
m3thx <mathieu.hatstatt@gmail.com>
2023-05-03 17:15:33 +0200

    03.05.23 Ajout commentaires, correction css et ajout compteur

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -8,9 +8,12 @@ import { SlideshowStyle } from './style'
 
 
 export const Slideshow = ({ ...accommodation }) => {
-    // const picturesLastIndex = accommodation.pictures.length - 1
-    // console.log(picturesLastIndex)
     const [indexPicture, setindexPicture] = useState(0)
+
+    // Gestion du click sur la flèche de gauche
+    // Si indexPicture est à zéro
+    // Passer l'index du tableau des images au indexPicture
+    // Sinon enlever 1 du indexPicture
     const HandleClickLeft = () => {
         if (indexPicture === 0) {
             setindexPicture(accommodation.pictures.length - 1)
@@ -18,6 +21,10 @@ export const Slideshow = ({ ...accommodation }) => {
             setindexPicture(indexPicture - 1)
         }
     }
+    // Gestion du click sur la flèche de droite
+    // Si indexPicture est à zéro
+    // Passer l'index du tableau des images au indexPicture
+    // Sinon ajouter 1 du indexPicture
     const HandleClickRight = () => {
         if (indexPicture === accommodation.pictures.length - 1) {
             setindexPicture(0)
@@ -25,17 +32,23 @@ export const Slideshow = ({ ...accommodation }) => {
             setindexPicture(indexPicture + 1)
         }
     }
+
     return (
-    
-            <SlideshowStyle>
-                <img className='slideshow_image' src={accommodation.pictures[indexPicture]} alt="Illustration du logement" />
-                {/* si une seule image les flèches n'apparaissent pas */}
-                {accommodation.pictures.length - 1 > 1 &&
-                    <div className='slideshow_arrowBox'>
-                        <img className='slideshow_arrow' src={arrow_left} alt="Arrow left" onClick={HandleClickLeft} />
-                        <img className='slideshow_arrow' src={arrow_right} alt="Arrow left" onClick={HandleClickRight} />
-                    </div>
-                }
-            </SlideshowStyle>
+        <SlideshowStyle>
+            <img className='slideshow_image' src={accommodation.pictures[indexPicture]} alt="Illustration du logement" />
+            {/* si une seule image les flèches n'apparaissent pas */}
+            {accommodation.pictures.length - 1 > 1 &&
+
+                <div className='slideshow_arrowBox'>
+                    <img className='slideshow_arrow' src={arrow_left} alt="Arrow left" onClick={HandleClickLeft} />
+                    <img className='slideshow_arrow' src={arrow_right} alt="Arrow left" onClick={HandleClickRight} />
+                </div>
+            }
+            {accommodation.pictures.length - 1 > 1 &&
+                <div className="pictureCount">
+                    <p>{indexPicture + 1}/{accommodation.pictures.length}</p>
+                </div>
+            }
+        </SlideshowStyle>
     )
 }
--- src/components/Slideshow/style.jsx
+++ src/components/Slideshow/style.jsx
@@ -1,34 +1,64 @@
 import styled from "styled-components"
+import colors from "../../styles/colors"
 
 export const SlideshowStyle = styled.div`
     position: relative;
-    height: 415px;
+    max-height: 415px;
     border-radius: 25px;
     display: flex;
     flex-direction: column;
     justify-content: center;
 
     @media (max-width: 1024px) {
-        height: 335px;
+        height: 255px;
     }
 
     .slideshow_image {
         object-fit: cover;
         border-radius: 25px;
         overflow: hidden;
-}
+    }
 
-.slideshow_arrowBox {
-    position: absolute;
-    width: 100%;
-    padding: 0 20px 0 20px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-}
+    .slideshow_arrowBox {
+        position: absolute;
+        width: 100%;
+        padding: 0 20px 0 20px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
     .slideshow_arrow {
         cursor: pointer;
-
+        
+        @media (max-width: 1024px) {
+            height: 60px;
+        }
+        @media (max-width: 768px) {
+            height: 40px;
+        }
+        @media (max-width: 480px) {
+            height: 20px;
+        }
     }
-`
 
+    .pictureCount {
+        position: absolute;
+        bottom: 25px;
+        /* left: 50%; */
+        width: 100%;
+
+        p{
+            font-family: Montserrat;
+            font-size: 18px;
+            font-weight: 500;
+            line-height: 26px;
+            letter-spacing: 0em;
+            text-align: center;
+            color: ${colors.white};
+        }
+        
+    }
+
+    
+`
